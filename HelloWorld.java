f3e8fa84ea1fe63cc966caae8d54e7380f67ef85
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2024-02-08 15:36:16 +0000

    Finished HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Displays Hello World message.
+ *
+ * @author Van
+ * @version 1.0
+ * @since 2024/02/07
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    // Displays message
+    System.out.println("Hello World!");
+    System.out.println("Hi Tamer! We're best* friends!");
+  }
+}
